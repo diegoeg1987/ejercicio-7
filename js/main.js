916428e4db2d60fe52dff9f8c53be67624732c89
Diego Gauto <dgauto1987@gmail.com>
2022-06-28 10:09:42 -0300

    Trabajo final

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+ let nombre 
+nombre= prompt ('Ingrese su nombre')
+console.log (nombre)
+
+let genero = prompt ('Por favor, ingrese su genero (hombre , mujer o no binario)')
+
+if (genero == "hombre") {
+    console.log ('Bienvenido')
+} 
+else if (genero == "mujer") {
+    console.log ('Bienvenida')
+} else {
+    console.log ('Bienvenidx')
+} 
